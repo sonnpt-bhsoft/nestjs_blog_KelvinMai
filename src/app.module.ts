5dcf0f27e93e29194d0e905cc8fdef4ef9a7b09a
Thai Son <sonnpt.bhsoft@gmail.com>
2020-10-24 11:14:30 +0700

    modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseConnectionService } from './database-connection/database-connection.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRootAsync({
     useClass: DatabaseConnectionService,
-  }), AuthModule, UserModule],
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService, DatabaseConnectionService],
 })
